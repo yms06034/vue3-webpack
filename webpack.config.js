c937a6cd2211600cf72e79d1c11a5e417fa70f55
Kim Sung Jin <yms06034@gmail.com>
2023-04-05 15:20:01 +0900

    Create Vue3 Webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const _require = id => require(require.resolve(id, { paths: [require.main.path] }))
+
+const path = _require('path')
+const HtmlPlugin = _require('html-webpack-plugin')
+const CopyPlugin = _require('copy-webpack-plugin')
+const { VueLoaderPlugin } = _require('vue-loader')
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      'assets': path.resolve(__dirname, 'src/assets')
+    }
+  },
+
+  entry: './src/main.js',
+
+  output: {
+    clean: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'vue-style-loader',
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
+    }),
+    new VueLoaderPlugin()
+  ],
+
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true
+  }
+}
